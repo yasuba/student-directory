7469b08dd9b8532d4edf82dee7dd05d4725c7a08
leopoldkwok <leopoldkwok@gmail.com>
2014-08-05 18:33:48 +0100

    Ex 5 - while

--- directory.rb
+++ directory.rb
@@ -1,45 +1,44 @@
 def print_header
-	print "The students of my cohort at Makers Academy\n"
-	print "-------------------\n"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def print_1(students)
-	students.each_with_index do |student, index|
-			if student[:name].start_with?("a") and student[:name].length < 13
-				print "#{index} #{student[:name]} (#{student[:cohort]} cohort)\n"
-				else
-				print nil
-			end
-			
-	end
+def print(students)
+  #students.each do |student|
+  count = students.count
+  	until count < 0 
+  		puts "#{students[count - 1][:name]} (#{students[count - 1][:cohort]} cohort)"
+  		count = count - 1
+  
+  end
 end
 
-def print_footer(names)
-	print "Overall, we have #{names.length} great students\n"
+def print_footer(names)    
+  puts "Overall, we have #{names.length} great students"
 end
 
 def input_students
-	print "Please enter the names of the students\n"
-	print "To finish, just hit return twice\n"
-	#create an empty array
-	students = []
-	#get the first name
-	name = gets.chomp
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		#add the student hash to the array
-		students << {:name => name, :cohort => :august}
-		puts "Now we have #{students.length} students"
-		#get another name from the user
-		name = gets.chomp
-	end
-	#return the array of students
-	students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  until name.empty? do    
+    # add the student hash to the array
+    students << {:name => name, :cohort => :november}    
+    puts "Now we have #{students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
 end
 
 students = input_students
 print_header
-print_1(students)
+print(students)
 print_footer(students)
 
 
