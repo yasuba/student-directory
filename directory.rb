9c9454bd175280580f3f66338e38091e8b23401c
leopoldkwok <leopoldkwok@gmail.com>
2014-08-05 17:07:51 +0100

    Exercises 3

--- directory.rb
+++ directory.rb
@@ -1,31 +1,3 @@
-#Let's put all students into an array
-students = [
-	{:name => "Javier Silverio", :cohort => :august},
-	{:name => "Elliot Lewis", :cohort => :august},
-	{:name => "Ben Tillett", :cohort => :august},
-	{:name => "Vincent Koch", :cohort => :august},
-	{:name => "Michelle Ballard", :cohort => :august},
-	{:name => "Nick Roberts", :cohort => :august},
-	{:name => "Tatiana Soukiassian", :cohort => :august},
-	{:name => "Mervé Silk", :cohort => :august},
-	{:name => "Albert Vallverdu", :cohort => :august},
-	{:name => "Lovis Schultze", :cohort => :august},
-	{:name => "Henry Stanley", :cohort => :august},
-	{:name => "Spike Lindsey", :cohort => :august},
-	{:name => "Ruth Earle", :cohort => :august},
-	{:name => "Andy Gates", :cohort => :august},
-	{:name => "Faisal Aydarus", :cohort => :august},
-	{:name => "Ethel Ng", :cohort => :august},
-	{:name => "Kevin Daniells", :cohort => :august},
-	{:name => "Maya Driver", :cohort => :august},
-	{:name => "Leopold Kwok", :cohort => :august},
-	{:name => "James McNeil",:cohort => :august},
-	{:name => "Jerome Pratt", :cohort => :august},
-	{:name => "David Wickes", :cohort => :august},
-	{:name => "Chris Oatley", :cohort => :august},
-	{:name => "Marc Singh", :cohort => :august}
-]
-
 def print_header
 	print "The students of my cohort at Makers Academy\n"
 	print "-------------------\n"
@@ -33,7 +5,11 @@ end
 
 def print_1(students)
 	students.each_with_index do |student, index|
-		print "#{index} #{student[:name]} (#{student[:cohort]} cohort)\n"
+			if student[:name].start_with?("a")
+				print "#{index} #{student[:name]} (#{student[:cohort]} cohort)\n"
+				else
+				print ""
+			end
 	end
 end
 
@@ -64,3 +40,7 @@ students = input_students
 print_header
 print_1(students)
 print_footer(students)
+
+
+
+
