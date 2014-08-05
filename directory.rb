d538dd56101a6b4fd953ebf8463b30ece5668fd6
leopoldkwok <leopoldkwok@gmail.com>
2014-08-05 14:23:16 +0100

    hashes and methods

--- directory.rb
+++ directory.rb
@@ -1,35 +1,46 @@
 #Let's put all students into an array
 students = [
-	"Javier Silverio"
-	"Elliot Lewis"
-	"Ben Tillett"
-	"Vincent Koch"
-	"Michelle Ballard"
-	"Nick Roberts"
-	"Tatiana Soukiassian"
-	"Mervé Silk"
-	"Albert Vallverdu"
-	"Lovis Schultze"
-	"Henry Stanley"
-	"Spike Lindsey"
-	"Ruth Earle"
-	"Andy Gates"
-	"Faisal Aydarus"
-	"Ethel Ng"
-	"Kevin Daniells"
-	"Maya Driver"
-	"Leopold Kwok"
-	"James McNeil"
-	"Jerome Pratt"
-	"David Wickes"
-	"Chris Oatley"
-	"Marc Singh"
+	{:name => "Javier Silverio", :cohort => :august},
+	{:name => "Elliot Lewis", :cohort => :august},
+	{:name => "Ben Tillett", :cohort => :august},
+	{:name => "Vincent Koch", :cohort => :august},
+	{:name => "Michelle Ballard", :cohort => :august},
+	{:name => "Nick Roberts", :cohort => :august},
+	{:name => "Tatiana Soukiassian", :cohort => :august},
+	{:name => "Mervé Silk", :cohort => :august},
+	{:name => "Albert Vallverdu", :cohort => :august},
+	{:name => "Lovis Schultze", :cohort => :august},
+	{:name => "Henry Stanley", :cohort => :august},
+	{:name => "Spike Lindsey", :cohort => :august},
+	{:name => "Ruth Earle", :cohort => :august},
+	{:name => "Andy Gates", :cohort => :august},
+	{:name => "Faisal Aydarus", :cohort => :august},
+	{:name => "Ethel Ng", :cohort => :august},
+	{:name => "Kevin Daniells", :cohort => :august},
+	{:name => "Maya Driver", :cohort => :august},
+	{:name => "Leopold Kwok", :cohort => :august},
+	{:name => "James McNeil",:cohort => :august},
+	{:name => "Jerome Pratt", :cohort => :august},
+	{:name => "David Wickes", :cohort => :august},
+	{:name => "Chris Oatley", :cohort => :august},
+	{:name => "Marc Singh", :cohort => :august}
 ]
-# and then print them
-puts "The students of my cohort at Makers Academy"
-puts "-------------------"
-students.each do |student|
-	puts student
+
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-------------------"
 end
-#finally, we print the total
-puts "Overall, we have #{students.length} great students"
+
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{students.length} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
