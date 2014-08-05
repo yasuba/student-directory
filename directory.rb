42f3d5cb0b714e20ccb833fa277179fd5eaca38e
leopoldkwok <leopoldkwok@gmail.com>
2014-08-05 15:34:26 +0100

    asking for user input

--- directory.rb
+++ directory.rb
@@ -38,9 +38,29 @@ def print(students)
 end
 
 def print_footer(names)
-	puts "Overall, we have #{students.length} great students"
+	puts "Overall, we have #{names.length} great students"
 end
-#nothing happens until we call the methods
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :august}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
