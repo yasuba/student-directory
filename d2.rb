200e527cd346b20d034e99fd2a7c178229bd5ed0
Maya Driver <mayadrivermd@gmail.com>
2014-08-07 18:07:26 +0100

    version 3

--- d2.rb
+++ d2.rb
@@ -1,12 +1,19 @@
-student_count = 7
+students = [
+"student1",
+"student2",
+"student3",
+"student4",
+"student5",
+"student6",
+"student7",
+]
+
 puts "The students of my cohort at Makers are:"
 puts "--------------"
-puts "student1"
-puts "student2"
-puts "student3"
-puts "student4"
-puts "student5"
-puts "student6"
-puts "student7"
-puts "Overall, we have #{student_count} great students" 
+
+students.each do |student|
+	puts student
+end
+
+"Overall, we have #{students.length} great students" 
 
