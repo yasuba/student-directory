172542fc345a93f13efbedd1972fbc3ce178b383
Maya Driver <mayadrivermd@gmail.com>
2014-08-07 17:56:16 +0100

    version 2

---
+++ d2.rb
@@ -0,0 +1,12 @@
+student_count = 7
+puts "The students of my cohort at Makers are:"
+puts "--------------"
+puts "student1"
+puts "student2"
+puts "student3"
+puts "student4"
+puts "student5"
+puts "student6"
+puts "student7"
+puts "Overall, we have #{student_count} great students" 
+
