6cff15e19cf83c5b09c3ba00cc4effade61f86fe
Maya Driver <mayadrivermd@gmail.com>
2014-08-09 16:35:42 +0100

    ex 8 missing data refactored

--- directory.rb
+++ directory.rb
@@ -1,65 +1,46 @@
-@students = []
+def input_students
+	puts "Please enter the names of the students and their cohort"
+	puts "To finish, just hit return twice"
+	students = []
+	answer = gets.chomp.downcase
+	name, cohort = answer.chomp.split(" ")
+	until answer.empty? do
+		students << {:name => name, :cohort => cohort}
+		puts "Now we have #{students.length} students."
+		answer = gets.chomp.downcase
+		name, cohort = answer.chomp.split(" ")
+	end
+	students
+	missing_answer(students)
+end
+
+def missing_answer(students)
+	students.each do|student|
+		if student[:cohort] == nil
+  	 	 student[:cohort] = "missing"
+  		end
+  	end
+end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+	puts "The students of my cohort at Makers are:"
+	puts "--------------".center(50)
 end
 
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+def print_students(students)
+	count = students.count - 1
+	until count < 0 
+		print "#{students[count - 1][:name]}".center(25)
+		puts "(#{students[count - 1][:cohort].to_sym})".center(25)
+		count = count - 1
+	end
 end
 
-def print_footer    
-  puts "Overall, we have #{@students.length} great students"
+def print_footer(students)
+	"Overall, we have #{students.length} great students" 
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
-    @students << {:name => name, :cohort => :november}    
-    puts "Now we have #{@students.length} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-end
-
-def print_menu    
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items  
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you mean, try again"
-  end
-end
-
-def interactive_menu  
-  loop do
-    print_menu        
-    process(gets.chomp)
-  end
-end
-
-interactive_menu
+students = input_students
+print_header
+print_students(students)
+print_footer(students)
