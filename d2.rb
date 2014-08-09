7318e88aa2c75538dcf7179e7214d07ba78d7d9d
Maya Driver <mayadrivermd@gmail.com>
2014-08-09 12:01:56 +0100

    ex7

--- d2.rb
+++ d2.rb
@@ -1,27 +1,26 @@
 def input_students
-	puts "Please enter the names of the students, their hobby and their favourite colour"
+	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	students = []
-	answer = gets.chomp
-	name, hobby, colour = answer.chomp.split(" ")
-	until answer.empty? do
-		students << {:name => name, :hobby => hobby, :colour => colour}
+	name = gets.chomp
+	until name.empty? do
+		students << {:name => name, :cohort => :august}
 		puts "Now we have #{students.length} students."
-		answer = gets.chomp
-		name, hobby, colour = answer.chomp.split(" ")
+		name = gets.chomp
 	end
 	students
 end
 
 def print_header
 	puts "The students of my cohort at Makers are:"
-	puts "--------------"
+	puts "--------------".center(50)
 end
 
 def print_students(students)
 	count = students.count - 1
 	until count < 0 
-		puts "#{students[count - 1][:name]} (likes #{students[count - 1][:hobby]}, favourite colour is #{students[count-1][:colour]})"
+		print "#{students[count - 1][:name]}".center(25)
+		puts "(#{students[count - 1][:cohort]})".center(25)
 		count = count - 1
 	end
 end
