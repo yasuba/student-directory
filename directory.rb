ec895ed5974317766316bfb9892304603f2ddc50
Maya Driver <mayadrivermd@gmail.com>
2014-08-10 23:14:13 +0100

    v7 interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,25 @@
+def interactive_menu
+	students = []
+	loop do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		selection = gets.chomp
+		case selection
+		when "1"
+			students = input_students
+		when "2"
+			print_header(students)
+			students_by_month(students)
+			print_footer(students)
+		when "9"
+			exit
+		else 
+			puts "I don't know what you meant, try again"
+		end
+	end
+end
+
 def input_students
 	puts "Please enter the names of the students and their cohort"
 	puts "To finish, just hit return twice"
@@ -57,7 +79,4 @@ def print_footer(students)
 	"Overall, we have #{students.length} great students" 
 end
 
-students = input_students
-print_header(students)
-students_by_month(students)
-print_footer(students)
+interactive_menu
