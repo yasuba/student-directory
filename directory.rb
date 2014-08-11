f8f15cb988337615febc1108de15a211c8d09cb8
Maya Driver <mayadrivermd@gmail.com>
2014-08-11 14:08:02 +0100

    ex 8 save students

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit"
 end
 
@@ -17,6 +18,8 @@ def process(selection)
 		input_students
 	when "2"
 		show_students(@students)
+	when "3"
+		save_students
 	when "9"
 		exit
 	else 
@@ -86,6 +89,16 @@ end
 
 def print_footer(students)
 	"Overall, we have #{@students.length} great students" 
+end
+
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
 end
 
 interactive_menu
